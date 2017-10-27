a95390c57e3875dccf0a0a7b56d75e7c33bd2e20
JUAN PABLO RIVAS HARO <juan.rivas@ibm.com>
2017-10-27 14:47:05 -0500

    Added nba_test_bot.py file

---
+++ nba_test_bot.py
@@ -0,0 +1,26 @@
+import praw
+import time
+
+reddit = praw.Reddit(client_id='HHZ5FFVA72bOFA',
+					 client_secret='d2vtaU2gMLhxZ9x6nfxFtWp-AS0',
+					 user_agent= 'This is a test bot',
+					 username='CoyoteTango_',
+					 password='Bloodline19rd')
+
+subreddit = reddit.subreddit('nba')
+keywords= ['stats']
+sleep_time = None
+
+
+for comment in reddit.subreddit('nba').stream.comments(): #Looking for 'stats player name'
+	for keyword in keywords:
+			if keyword in comment.body.split():
+				try:
+					new_coment = comment.reply('Shhhhh... all you gotta do is trust The Process.')
+					print(new_coment.body)
+				except praw.exceptions.APIException as e:
+					time.sleep(600)
+
+
+if __name__ == '__main__':
+	main()
